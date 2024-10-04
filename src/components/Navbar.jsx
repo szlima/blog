2ca991ce1074a0231738ce136ea9e3218f0cc1e1
Suzane Lima <suzanelima@outlook.com>
2024-10-04 17:41:29 -0300

    Ativação do menu da página em telas menores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,26 @@ import { Link } from 'react-router-dom';
 
 function Navbar(){
 
+    const toggleMenu= e =>
+        e.target.parentElement.classList.toggle('navbar__list--active');
+
+    const closeMenu= e =>
+        e.target.parentNode.parentNode.classList.remove('navbar__list--active');
+
     return (
         <nav className='navbar'>
             <ul className='navbar__list'>
-                <li className='navbar__list__item'>Menu</li>
-                <li className='navbar__list__item'>
+                <li className='navbar__list__item' onClick={toggleMenu}>Menu</li>
+                <li className='navbar__list__item' onClick={closeMenu}>
                     <Link to='/'>Home</Link>
                 </li>
-                <li className='navbar__list__item'>
+                <li className='navbar__list__item' onClick={closeMenu}>
                     <Link to='/about'>About</Link>
                 </li>
-                <li className='navbar__list__item'>
+                <li className='navbar__list__item' onClick={closeMenu}>
                     <Link to='/faq'>FAQ</Link>
                 </li>
-                <li className='navbar__list__item'>
+                <li className='navbar__list__item' onClick={closeMenu}>
                     <Link to='/contact'>Contact</Link>
                 </li>
             </ul>
